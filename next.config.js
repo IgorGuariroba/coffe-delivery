22b6e6fda08f3d309f7e1d8851ac502ef8438d34
igor.guariroba <igor.guariroba@jsl.com.br>
2023-06-24 18:41:02 -0300

    trailingComma all

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  pageExtensions: [
-    'page.tsx',
-    'api.ts',
-    'api.tsx'
-  ]
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
