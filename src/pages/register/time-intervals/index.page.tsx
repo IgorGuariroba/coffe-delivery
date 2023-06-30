50e734c176fdb3223eaf953a571fdd005e820130
igor.guariroba <igor.guariroba@jsl.com.br>
2023-06-30 16:05:56 -0300

    "Add time intervals selection in registration process
    A new page was created for the registration process that adds the functionality to select available time intervals for each day of the week. This was achieved through creating a new 'time-intervals' index.page.tsx and styles.ts files. Additionally, a session function was added to the [...nextauth].api.ts file to ensure user sessions are considered when selecting these time intervals. This change allows users to define their availability during the registration process, providing a more accurate and detailed user profile."

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,64 @@
+import { Container, Header } from '../styles'
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntevalsContainer,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+
+export default function TimeIntervals() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá!</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form">
+        <IntevalsContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntevalsContainer>
+
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
